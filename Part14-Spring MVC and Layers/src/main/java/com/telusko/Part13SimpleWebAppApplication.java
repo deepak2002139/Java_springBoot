6915d73567c4ff386c7efe32274ab3f244359011
Deepak Kumar <deepak.naman2002@gmail.com>
2025-07-22 23:25:39 +0530

    spring MVC

---
+++ Part14-Spring MVC and Layers/src/main/java/com/telusko/Part13SimpleWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.telusko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part13SimpleWebAppApplication 
+{
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(Part13SimpleWebAppApplication.class, args);
+    }
+
+}
